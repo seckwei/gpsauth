c49f15a5413811e971f03b78e8f77385dc30fe0e
SWLim <seckwei93@gmail.com>
2015-06-29 00:55:44 +0100

    New files

--- Demo Login/login.js
+++ Demo Login/login.js
@@ -4,13 +4,23 @@ var login = {
 
 		var obj = {
 			username : $("#username").val(),
-			clientid : $("#clientid").val()
+			clientid : "innovationwarehouse"
 		}
 
+		console.log(JSON.stringify(obj));
+
 		$.ajax({
-
+			type: "POST",
+		    url: "http://192.168.15.181:9000/auth",
+		    data: JSON.stringify(obj),
+		    dataType: "json",
+		    contentType: "application/json; charset=utf-8",
+		    success: function(result){
+		      console.log(result);
+		    },
+		    error: function(XMLHttpRequest, textStatus, errorThrown) {
+		       console.log(XMLHttpRequest, textStatus, errorThrown);
+		    }
 		});
-
-		alert(obj);
 	}
 };
